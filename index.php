4bb0f82448929247da5e53766eb6fd099d726cc2
lefa Motloung <lefacalvin.m@gmail.com>
2017-03-07 05:53:50 -0600

    hi

--- index.php
+++ index.php
@@ -12,8 +12,10 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
+				<h1 id = "message"><?php echo "Talk to me man...."; ?></h1>
+				
+				
+				<p class='description'></p> Thanks for talking to me <span class="blue"> i understand!!</span>.
 			</td>
 		</tr>
 	</table>
